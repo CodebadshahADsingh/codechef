df4862a60165f8647ef4a90fecb2c61c051b29a6
AMANDEEP SINGH <83573338+CodebadshahADsingh@users.noreply.github.com>
2022-08-31 08:30:05 +0530

    Add files via upload

---
+++ CARTRIP.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	 int a;
+	 int b=3000;
+	 scanf("%d",&a);
+	 if(a<=300){
+	     printf("%d\n",b);
+	 }
+	 else{
+	     
+	     printf("%d\n",a*10);
+	 }
+	 
+	    
+	    
+	}
+	return 0;
+}
+
